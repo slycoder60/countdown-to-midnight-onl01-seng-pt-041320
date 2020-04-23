a937037cde9ed20190b1c58eca5e83e0ab544c4a
Omar Farah <farah.omar8787@gmail.com>
2020-04-23 04:28:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,14 @@ def countdown(int)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+  end
+
+end
